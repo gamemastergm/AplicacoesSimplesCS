bab40f575a3e611d22dfd29061f4b8f8f7d00506
Felipe Diniz <79574991+gamemastergm@users.noreply.github.com>
2022-08-16 23:40:34 -0300

    Add Project
    smallaplications C#

---
+++ SmallAplications/SmallAplications/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine(fibonacci(10, 0, 1));
+Console.WriteLine(calculadoraSimples(10, 2, "dividir"));
+
+string fibonacci(int x, int y, int z)
+{
+    return y < x ? (Convert.ToString(y) + "\n") + fibonacci(x, z, y + z) : "";
+    //return x <= 1 ? 1 : fibonacci(x - 1) + fibonacci(x - 2);
+}
+
+string calculadoraSimples(int x, int y, string choose)
+{
+    return true ? (choose == "somar" ? Convert.ToString(x + y):"") + (choose == "diminuir" ? Convert.ToString(x - y): "") + (choose == "multiplicar" ? Convert.ToString(x * y): "") + (choose == "dividir" ? Convert.ToString(x / y): "") : "Saida";
+}
